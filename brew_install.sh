b2b45fedcd00be89fb6eeecdf75f123ecfdd4dca
ShiBin <15519900807@qq.com>
2019-07-02 01:00:38 +0800

    update

--- brew_install.sh
+++ brew_install.sh
@@ -6,7 +6,7 @@ echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebre
 source ~/.zshrc
 
 # Install CMD
-brew install maven tree aria2 scala wget go cheat htop neofetch netcat fzf netdata ranger tmux zsh-autosuggestions zsh-completions  maven nginx kafka mycli redis mysql cmatrix jeantil/autokbisw/autokbisw
+brew install maven gradle tree pstree aria2 scala wget go cheat htop neofetch netcat fzf netdata ranger tmux zsh-autosuggestions zsh-completions  maven nginx kafka mycli redis mysql cmatrix jeantil/autokbisw/autokbisw
 
 # Install GUI software
 brew cask install qq wechat wpsoffice iterm2 fliqlo alfred visual-studio-code typora thunder switchhosts docker teamviewer wireshark calibre iina anki shadowsocksx-ng paw postman omnidisksweeper betterzip
--- sync.sh
+++ sync.sh
@@ -6,3 +6,4 @@ rsync -auv ~/.m2/settings.xml               ./.m2/
 rsync -auv ~/.gradle/init.gradle            ./.gradle/
 rsync -auv ~/.vim_runtime/my_configs.vim    ./.vim_runtime/
 rsync -auv ~/.zshrc                         ./
+rsync -auv ~/Alfred                         ./
